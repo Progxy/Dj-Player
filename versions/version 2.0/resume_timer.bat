e62d3dbfcbeb1155aa92ec4a47a0755254bfb2e6
Progxy <70652793+Progxy@users.noreply.github.com>
2020-09-06 16:04:23 +0200

    Update resume_timer.bat

--- versions/version 2.0/resume_timer.bat
+++ versions/version 2.0/resume_timer.bat
@@ -1,8 +1,12 @@
 @echo off
 
-set /p Build=<"C:\Users\Emanuele\Desktop\python\progetti\get_pos.txt"
+set /p Build=<path_get_pos.txt
 
-set /p Build2=<"C:\Users\Emanuele\Desktop\python\progetti\n_loop.txt"
+rem replace with the place of the path of get_pos.txt (the position must be equal for all the get_pos.txt)
+
+set /p Build2=<path_n_loop.txt
+
+rem replace with the place of the path of n_loop.txt (the position must be equal for all the n_loop.txt)
 
 set /a n_loop = Build2
 
@@ -47,7 +51,9 @@ set /a n_loop = n_loop - 1
 
 if /i %n_loop% == 0 (goto finish)
 
-set /p Build3=<"C:\Users\Emanuele\Desktop\python\progetti\fermati.txt"
+set /p Build3=<path_stop.txt
+
+rem replace with the place of the path of stop.txt (the position must be equal for all the stop.txt)
 
 set /a stop = Build3
 
